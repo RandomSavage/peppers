0fb76cb65441c95ca03dcc15ebabe40c072ce598
RandomSavage <50834695+RandomSavage@users.noreply.github.com>
2021-02-21 14:24:03 -0500

    Leaflet And OSM Installed

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 
+  // initialize Leaflet
+let myMap = L.map('map-box').setView({lon: 0, lat: 0}, 2);
 
-const navToggle = document.querySelector(".nav-toggle");
-const side = document.querySelector(".side");
+ // add the OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+  }).addTo(myMap);
 
-navToggle.addEventListener('click', () => {
-  side.classList.toggle("show-side");
-});
+// show the scale bar on the lower left corner
+L.control.scale().addTo(myMap);
+
+// show a marker on the map
+L.marker({lon: 0, lat: 0}).bindPopup('<h1>Make it clap to this!</h1>').addTo(myMap);
+console.log(typeof(myMap));
+
+// let circle = L.circle({lon: 0, lat: 0}, {color: 'red', fillColor: '#f03', fillOpacity: 0.5, radius: 500}).addTo(myMap);
+// circle.bindPopup("I am a circle.");
+
+let polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047],
+  [51.519505, -0.066948]
+]).addTo(myMap);
+
+let popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(myMap);
+}
+
+myMap.on('click', onMapClick);
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,7 +1,32 @@
 "use strict";
 
-var navToggle = document.querySelector(".nav-toggle");
-var side = document.querySelector(".side");
-navToggle.addEventListener('click', function () {
-  side.classList.toggle("show-side");
-});
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+// initialize Leaflet
+var myMap = L.map('map-box').setView({
+  lon: 0,
+  lat: 0
+}, 2); // add the OpenStreetMap tiles
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(myMap); // show the scale bar on the lower left corner
+
+L.control.scale().addTo(myMap); // show a marker on the map
+
+L.marker({
+  lon: 0,
+  lat: 0
+}).bindPopup('<h1>Make it clap to this!</h1>').addTo(myMap);
+console.log(_typeof(myMap)); // let circle = L.circle({lon: 0, lat: 0}, {color: 'red', fillColor: '#f03', fillOpacity: 0.5, radius: 500}).addTo(myMap);
+// circle.bindPopup("I am a circle.");
+
+var polygon = L.polygon([[51.509, -0.08], [51.503, -0.06], [51.51, -0.047], [51.519505, -0.066948]]).addTo(myMap);
+var popup = L.popup();
+
+function onMapClick(e) {
+  popup.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(myMap);
+}
+
+myMap.on('click', onMapClick);
